ff193a218b1d4418579ca7c89936bdd3247274e9
Imhandegbelo <okogbe@outloo.com>
2022-11-10 01:46:51 +0100

    put some finishing touches

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
